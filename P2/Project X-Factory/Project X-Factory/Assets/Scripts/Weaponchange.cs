08e7ea06a4dee19fbaeef635c053b7fcf6eb7bd3
Dominik Beens <dominik.beens@hotmail.nl>
2016-11-21 14:03:12 +0100

    First commit

---
+++ P2/Project X-Factory/Project X-Factory/Assets/Scripts/Weaponchange.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weaponchange : MonoBehaviour
+{
+
+    public int currentWeapon;
+    public Transform[] weapons;
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            changeWeapon(1);
+        }
+    }
+
+    public void changeWeapon(int num)
+    {
+        currentWeapon++;  
+        currentWeapon %= weapons.Length;
+
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if (i == num)
+                weapons[i].gameObject.SetActive(true);
+            else
+                weapons[i].gameObject.SetActive(false);
+        }
+    }
+}
